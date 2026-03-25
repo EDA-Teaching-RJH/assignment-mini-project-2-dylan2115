d767ccde08ab54b73d459ac9c0e515cb2e7a8e59
dylan lewis <djl51@kent.ac.uk>
2026-03-25 20:09:44 +0000

    main v3

--- log_analyzer_project/main.py
+++ log_analyzer_project/main.py
@@ -1,15 +1,18 @@
 #main.py
-#entry point for log file analyzer project 
+#entry point for log file analyzer project
+from analyzer import LogAnalyzer, ErrorLogAnalyzer
+
 
 def main():
     print("log file analyzer starting ...")
+# Create a LogAnalyzer for the sample log file
 
 
     analyzer = loganalyzer("logs/sample.log")
     analyzer.load()
 
     print("total log entries:",len(analyzer.entries))
-
+# Create an ErrorLogAnalyzer for the same file
     error_analyzer = ErrorLogAnalyzer("logs/sample.log")
     error_analyzer.load()
 
