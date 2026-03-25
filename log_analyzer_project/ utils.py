a0d0ef9a7e3f48ad4f11a68e6e43b26fa3f99bf6
dylan lewis <djl51@kent.ac.uk>
2026-03-25 13:10:27 +0000

    utils v2

--- log_analyzer_project/ utils.py
+++ log_analyzer_project/ utils.py
@@ -1,2 +1,10 @@
-def placeHolder():
-    return "utils ready"
+import re 
+
+#functions for regex extraction 
+
+def extraxt_timestamp(line: str):
+#this extracxts the time stamp from the sample log
+    pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}
+    match = re.search(pattern, line)
+    return match.groupe(0) if match else none 
+
