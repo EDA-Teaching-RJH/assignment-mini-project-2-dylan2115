d9eb143be85ff2b45e874f105b5044dcb66936a8
dylan lewis <djl51@kent.ac.uk>
2026-03-25 19:44:14 +0000

    main v2

--- log_analyzer_project/main.py
+++ log_analyzer_project/main.py
@@ -4,5 +4,16 @@
 def main():
     print("log file analyzer starting ...")
 
+
+    analyzer = loganalyzer("logs/sample.log")
+    analyzer.load()
+
+    print("total log entries:",len(analyzer.entries))
+
+    error_analyzer = ErrorLogAnalyzer("logs/sample.log")
+    error_analyzer.load()
+
+    print("Total error entries:", len(error_analyzer.get_errors()))
+
 if __name__ == "__main__":
     main()
