d4b3e0640d14bbed8b89a913e42e9a5c9f1590dd
dylan lewis <djl51@kent.ac.uk>
2026-03-26 17:28:28 +0000

    analyzer v9 and v10 sorry sir forgot to do it

--- log_analyzer_project/main.py
+++ log_analyzer_project/main.py
@@ -8,16 +8,36 @@ def main():
 # Create a LogAnalyzer for the sample log file
 
 
-    analyzer = LogAnalyzer("log_analyzer_project/logs/sample.log")
-    analyzer.load()
+analyzer = LogAnalyzer("log_analyzer_project/logs/sample.log")
+analyzer.load()
 #loads the files 
-    print("total log entries:",len(analyzer.entries))
+print("total log entries:",len(analyzer.entries))
 #prints total files 
 # Create an ErrorLogAnalyzer for the same file
-    error_analyzer = ErrorLogAnalyzer("log_analyzer_project/logs/sample.log")
-    error_analyzer.load()
+error_analyzer = ErrorLogAnalyzer("log_analyzer_project/logs/sample.log")
+error_analyzer.load()
 #loads errors only 
-    print("Total error entries:", len(error_analyzer.get_errors()))
+print("Total error entries:", len(error_analyzer.get_errors()))
+
+ip_counts = analyzer.count_by_ip()
+print("\nRequests per IP:")
+for ip, count in ip_counts.items():
+    print(f"   {ip}: {count}")
+#count requests per ip
+
+url_counts = analyzer.count_by_url()
+print("\nRequests per URL:")
+for url, count in url_counts.items():
+    print(f"   {url}: {count}")
+#counts requests per url
+
+error_code_counts = error_analyzer.count_by_error_code()
+print("\nErrors by code:")
+for code, count in error_code_counts.items():
+    print(f"   {code}: {count}")
+#counts requests errorcode 
+
+
 
 if __name__ == "__main__":
     main()
