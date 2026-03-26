4a63a73527432eead6407d20e03330a09fd3ad56
dylan lewis <djl51@kent.ac.uk>
2026-03-26 19:19:55 +0000

    main v10

--- log_analyzer_project/main.py
+++ log_analyzer_project/main.py
@@ -4,40 +4,42 @@ from .analyzer import LogAnalyzer, ErrorLogAnalyzer
 #impoerts the classes 
 
 def main():
-    print(">>> RUN MAKER 12345",flush=True)
+    import inspect
+   
+    print("Initializing Log File Analyzer...",flush=True)
 # Create a LogAnalyzer for the sample log file
 
 
-analyzer = LogAnalyzer("log_analyzer_project/logs/sample.log")
-analyzer.load()
+    analyzer = LogAnalyzer("log_analyzer_project/logs/sample.log")
+    analyzer.load()
 #loads the files 
-print("Total log entries:",len(analyzer.entries))
+    print("Total log entries:",len(analyzer.entries))
 #prints total files 
 # Create an ErrorLogAnalyzer for the same file
-error_analyzer = ErrorLogAnalyzer("log_analyzer_project/logs/sample.log")
-error_analyzer.load()
+    error_analyzer = ErrorLogAnalyzer("log_analyzer_project/logs/sample.log")
+    error_analyzer.load()
 #loads errors only 
-print("Total error entries:", len(error_analyzer.get_errors()))
+    print("Total error entries:", len(error_analyzer.get_errors()))
 
-ip_counts = analyzer.count_by_ip()
-print("\nRequests per IP:")
-for ip, count in ip_counts.items():
-    print(f"   {ip}: {count}")
+    ip_counts = analyzer.count_by_ip()
+    print("\nRequests per IP:")
+    for ip, count in ip_counts.items():
+        print(f"   {ip}: {count}")
 #count requests per ip
 
-url_counts = analyzer.count_by_url()
-print("\nRequests per URL:")
-for url, count in url_counts.items():
-    print(f"   {url}: {count}")
+    url_counts = analyzer.count_by_url()
+    print("\nRequests per URL:")
+    for url, count in url_counts.items():
+        print(f"   {url}: {count}")
 #counts requests per url
 
-error_code_counts = error_analyzer.count_by_error_code()
-print("\nErrors by code:")
-for code, count in error_code_counts.items():
-    print(f"   {code}: {count}")
+    error_code_counts = error_analyzer.count_by_error_code()
+    print("\nErrors by code:")
+    for code, count in error_code_counts.items():
+        print(f"   {code}: {count}")
 #counts requests errorcode 
 
 
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
