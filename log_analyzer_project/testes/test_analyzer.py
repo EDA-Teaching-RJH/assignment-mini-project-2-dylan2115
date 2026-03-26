df69eb349bbb9cd73d1d935da8a89bebf43f553d
dylan lewis <djl51@kent.ac.uk>
2026-03-26 20:48:19 +0000

    test v3

--- log_analyzer_project/testes/test_analyzer.py
+++ log_analyzer_project/testes/test_analyzer.py
@@ -2,3 +2,9 @@
 # Tests for the Log File Analyzer
 
 from analyzer import LogAnalyzer, ErrorLogAnalyzer
+
+def test_load_entries():
+    analyzer = LogAnalyzer("Log_analyzer_project/logs/sample.log")
+    analyzer.load()
+    assert len(analyzer.entries) > 0
+
